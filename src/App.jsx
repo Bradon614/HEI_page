5ba44de50acdd4e4f510d76939ee38e9ab590600
Terminator <hei.christiano@gmail.com>
2025-07-09 14:20:51 +0300

    build navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from './components/NavBar'
+import Aside from './components/Aside'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <Navbar/>
+      <Aside/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+
+export default function Navbar() {
+
+    return <>
+        <div className="navbar">
+           
+            <ul className="liste">
+                <li>ACCEUIL</li>
+                <li>ACTUALITE</li>
+                <li>BOURSE D'ETUDES</li>
+                <li>INSCRIPTION</li>
+            </ul>
+
+            <button>INTRANET</button>
+
+        </div>
+    </>
+}   
