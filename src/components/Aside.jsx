72b96e878a1beb7285ac38cf158ce3dcb3f9ef63
Terminator <christianobradon@gmail.com>
2025-07-12 02:31:26 +0300

    refactor aside

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,9 +1,10 @@
 export default function Aside (){
     return <>
     <div className="aside">
-        <div>
+        <div className="sous_aside">
             <h1>Haute École d'Informatique</h1>
-            <p>« L'éducation est l'arme la plus puissante pour changer le monde » selon Nelson Mandela. « L'éducation dans l'informatique est une arme très puissante pour lutter contre la pauvreté à Madagascar » selon HEI
+            <p>« L'éducation est l'arme la plus puissante pour changer le monde » selon Nelson Mandela. </p> 
+            <p>« L'éducation dans l'informatique est une arme très puissante pour lutter contre la pauvreté à Madagascar » selon HEI
             C'est notre mission.</p>
             <div className="listButton">
                 <button>Inscrivez-vous ici</button>
