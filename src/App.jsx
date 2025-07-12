a25e5214086d7566cdd3c1c1ba67e86915384d68
Toms <hei.damien.3@gmail.com>
2025-07-12 13:19:25 +0200

    Ajout de fontawesome en tant que package

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './components/NavBar'
 import Aside from './components/Aside'
 import './App.css'
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import About from './components/about'
 
 function App() {
